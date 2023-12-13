c4c19c9d387195f93c23c616eac85fb720aece51
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2023-12-13 07:59:30 +0200

    Editing string functions var

--- string_functions4.c
+++ string_functions4.c
@@ -1,25 +1,22 @@
 #include "shell.h"
 
 /**
- * **strtow - splits a string into words. Repeat delimiters are ignored
+ * **strtow2 - splits a string into words
  * @str: the input string
- * @d: the delimeter string
+ * @delim: the delimeter
  * Return: a pointer to an array of strings, or NULL on failure
  */
-
-char **strtow(char *str, char *d)
+char **strtow2(char *str, char delim)
 {
 	int i, j, k, m, numwords = 0;
 	char **s;
 
 	if (str == NULL || str[0] == 0)
 		return (NULL);
-	if (!d)
-		d = " ";
 	for (i = 0; str[i] != '\0'; i++)
-		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
+		if ((str[i] != delim && str[i + 1] == delim) ||
+		    (str[i] != delim && !str[i + 1]) || str[i + 1] == delim)
 			numwords++;
-
 	if (numwords == 0)
 		return (NULL);
 	s = malloc((1 + numwords) * sizeof(char *));
@@ -27,10 +24,10 @@ char **strtow(char *str, char *d)
 		return (NULL);
 	for (i = 0, j = 0; j < numwords; j++)
 	{
-		while (is_delim(str[i], d))
+		while (str[i] == delim && str[i] != delim)
 			i++;
 		k = 0;
-		while (!is_delim(str[i + k], d) && str[i + k])
+		while (str[i + k] != delim && str[i + k] && str[i + k] != delim)
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
@@ -49,22 +46,24 @@ char **strtow(char *str, char *d)
 }
 
 /**
- * **strtow2 - splits a string into words
+ * **strtow - splits a string into words. Repeat delimiters are ignored
  * @str: the input string
- * @d: the delimeter
+ * @delim: the delimeter string
  * Return: a pointer to an array of strings, or NULL on failure
  */
-char **strtow2(char *str, char d)
+char **strtow(char *str, char *delim)
 {
 	int i, j, k, m, numwords = 0;
 	char **s;
 
 	if (str == NULL || str[0] == 0)
 		return (NULL);
+	if (!delimd)
+		delim = " ";
 	for (i = 0; str[i] != '\0'; i++)
-		if ((str[i] != d && str[i + 1] == d) ||
-		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
+		if (!is_delim(str[i], delim) && (is_delim(str[i + 1], delim) || !str[i + 1]))
 			numwords++;
+
 	if (numwords == 0)
 		return (NULL);
 	s = malloc((1 + numwords) * sizeof(char *));
@@ -72,10 +71,10 @@ char **strtow2(char *str, char d)
 		return (NULL);
 	for (i = 0, j = 0; j < numwords; j++)
 	{
-		while (str[i] == d && str[i] != d)
+		while (is_delim(str[i], d))
 			i++;
 		k = 0;
-		while (str[i + k] != d && str[i + k] && str[i + k] != d)
+		while (!is_delim(str[i + k], delim) && str[i + k])
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
