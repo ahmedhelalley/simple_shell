493c74d21404ca8c9a6194353b730ecb93f2b2ce
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2023-12-13 07:58:20 +0200

    more emulators and chain

---
+++ builtin_emulators2.c
@@ -0,0 +1,115 @@
+#include "shell.h"
+
+/**
+ * unset_alias - sets alias to string
+ * @info: parameter struct
+ * @s: the string alias
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int unset_alias(info_t *info, char *s)
+{
+	char *pt, ch;
+	int back;
+
+	pt = _strchr(s, '=');
+	if (!pt)
+		return (1);
+	ch = *pt;
+	*pt = 0;
+	back = delete_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, s, -1)));
+	*pt = ch;
+	return (back);
+}
+
+/**
+ * set_alias - sets alias to string
+ * @info: parameter struct
+ * @s: the string alias
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int set_alias(info_t *info, char *s)
+{
+	char *pt;
+
+	pt = _strchr(s, '=');
+	if (!pt)
+		return (1);
+	if (!*++pt)
+		return (unset_alias(info, s));
+
+	unset_alias(info, s);
+	return (add_node_end(&(info->alias), s, 0) == NULL);
+}
+
+/**
+ * _myhistory - displays the history list, one command by line, preceded
+ *              with line numbers, starting at 0.
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _myhistory(info_t *info)
+{
+	print_list(info->history);
+	return (0);
+}
+
+/**
+ * _myalias - mimics the alias builtin (man alias)
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _myalias(info_t *info)
+{
+	int i = 0;
+	char *pt = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		pt = _strchr(info->argv[i], '=');
+		if (pt)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
+}
+
+/**
+ * print_alias - prints an alias string
+ * @node: the alias node
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int print_alias(list_t *node)
+{
+	char *pt = NULL, *a = NULL;
+
+	if (node)
+	{
+		pt = _strchr(node->str, '=');
+		for (a = node->str; a <= pt; a++)
+			_putchar(*a);
+		_putchar('\'');
+		_puts(pt + 1);
+		_puts("'\n");
+		return (0);
+	}
+	return (1);
+}
---
+++ chain.c
@@ -0,0 +1,154 @@
+#include "shell.h"
+
+/**
+ * check_chain - checks we should continue chaining based on last status
+ * @info: the parameter struct
+ * @buff: the char buffer
+ * @pt: address of current position in buff
+ * @j: starting position in buff
+ * @len: length of buff
+ *
+ * Return: Void
+ */
+void check_chain(info_t *info, char *buff, size_t *pt, size_t j, size_t len)
+{
+	size_t i = *pt;
+
+	if (info->cmd_buf_type == CMD_AND)
+	{
+		if (info->status)
+		{
+			buff[j] = 0;
+			i = len;
+		}
+	}
+	if (info->cmd_buf_type == CMD_OR)
+	{
+		if (!info->status)
+		{
+			buff[j] = 0;
+			i = len;
+		}
+	}
+
+	*pt = i;
+}
+
+/**
+ * is_chain - test if current char in buffer is a chain delimeter
+ * @info: the parameter struct
+ * @buff: the char buffer
+ * @pt: address of current position in buf
+ *
+ * Return: 1 if chain delimeter, 0 otherwise
+ */
+int is_chain(info_t *info, char *buff, size_t *pt)
+{
+	size_t k = *pt;
+
+	if (buff[k] == '|' && buff[k + 1] == '|')
+	{
+		buff[k] = 0;
+		k++;
+		info->cmd_buf_type = CMD_OR;
+	}
+	else if (buff[k] == '&' && buff[k + 1] == '&')
+	{
+		buff[k] = 0;
+		k++;
+		info->cmd_buf_type = CMD_AND;
+	}
+	else if (buff[k] == ';') /* found end of this command */
+	{
+		buff[k] = 0; /* replace semicolon with null */
+		info->cmd_buf_type = CMD_CHAIN;
+	}
+	else
+		return (0);
+	*pt = k;
+	return (1);
+}
+
+/**
+ * replace_alias - replaces an aliases in the tokenized string
+ * @info: the parameter struct
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replace_alias(info_t *info)
+{
+	int j;
+	list_t *node;
+	char *pt;
+
+	for (j = 0; j < 10; j++)
+	{
+		node = node_starts_with(info->alias, info->argv[0], '=');
+		if (!node)
+			return (0);
+		free(info->argv[0]);
+		p = _strchr(node->str, '=');
+		if (!pt)
+			return (0);
+		p = _strdup(pt + 1);
+		if (!pt)
+			return (0);
+		info->argv[0] = pt;
+	}
+	return (1);
+}
+
+/**
+ * replace_string - replaces string
+ * @olds: address of old string
+ * @news: new string
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replace_string(char **olds, char *news)
+{
+	free(*olds);
+	*olds = news;
+	return (1);
+}
+
+/**
+ * replace_vars - replaces vars in the tokenized string
+ * @info: the parameter struct
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replace_vars(info_t *info)
+{
+	int j = 0;
+	list_t *node;
+
+	for (j = 0; info->argv[j]; j++)
+	{
+		if (info->argv[j][0] != '$' || !info->argv[j][1])
+			continue;
+
+		if (!_strcmp(info->argv[j], "$?"))
+		{
+			replace_string(&(info->argv[j]),
+				_strdup(convert_number(info->status, 10, 0)));
+			continue;
+		}
+		if (!_strcmp(info->argv[j], "$$"))
+		{
+			replace_string(&(info->argv[j]),
+				_strdup(convert_number(getpid(), 10, 0)));
+			continue;
+		}
+		node = node_starts_with(info->env, &info->argv[j][1], '=');
+		if (node)
+		{
+			replace_string(&(info->argv[j]),
+				_strdup(_strchr(node->str, '=') + 1));
+			continue;
+		}
+		replace_string(&info->argv[j], _strdup(""));
+
+	}
+	return (0);
+}
