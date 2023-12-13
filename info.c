eee9d1f3f5a2ae5ffeda02a107aaea08a0288d21
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2023-12-13 20:58:53 +0200

    info file

---
+++ info.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+
+/**
+ * set_info - initializes info_t struct
+ * @info: struct address
+ * @av: argument vector
+ */
+void set_info(info_t *info, char **av)
+{
+	int j = 0;
+
+	info->fname = av[0];
+	if (info->arg)
+	{
+		info->argv = strtow(info->arg, " \t");
+		if (!info->argv)
+		{
+
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = _strdup(info->arg);
+				info->argv[1] = NULL;
+			}
+		}
+		for (j = 0; info->argv && info->argv[j]; j++)
+			;
+		info->argc = j;
+
+		replace_alias(info);
+		replace_vars(info);
+	}
+}
+
+/**
+ * clear_info - initializes info_t struct
+ * @info: struct address
+ */
+void clear_info(info_t *info)
+{
+	info->arg = NULL;
+	info->argv = NULL;
+	info->path = NULL;
+	info->argc = 0;
+}
+
